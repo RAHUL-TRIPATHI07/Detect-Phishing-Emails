d18a1114287cac01b73fc29a517f70346e729879
RAHUL-TRIPATHI07 <tripathidrahul@gmail.com>
2026-09-13 18:07:05 +0530

    Configure production API URL

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://detect-phishing-emails-3.onrender.com";
 
 // =========================================================
 // DOM ELEMENTS
